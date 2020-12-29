1695cbcec95fda93372ea5616b2e096d046ca191
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-29 13:21:00 +0530

    ADD:To calculate employee wage DailyEmpWageUC2

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,13 +3,18 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation");
-		int present=1;
+		int EMP_RATE_PER_HR=20;
+		int PRESENT=1;
+		int empHrs=0;
+		int empWage=0;
 		int attendanceCheck=(int)(Math .random()*10%2);
 
-		if (attendanceCheck==present)
-			System.out.println("Employee is present");
+		if (attendanceCheck==PRESENT)
+			empHrs=8;
 		else
-			System.out.println("Employee is absent");
+			empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HR;
+		System.out.println("Employee Wage: "+empWage);
 	}
 }
 
