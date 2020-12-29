79c60290c18279006586c2a2e35568af4f8e9d26
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-29 13:04:52 +0530

    ADD:To check employee is present or absent EmployeeAttendanceUC1

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,7 +2,14 @@ public class EmpWageBuilder {
 
 	public static void main(String[] args) {
 
-	System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("Welcome to Employee Wage Computation");
+		int present=1;
+		int attendanceCheck=(int)(Math .random()*10%2);
+
+		if (attendanceCheck==present)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 	}
 }
 
