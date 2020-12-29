899c67e429df6966a9cf8379ae40b4e1154d3b9f
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-29 14:12:11 +0530

    ADD:To calculate monthly employee wage UC5

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,29 +5,37 @@ public class EmpWageBuilder
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
 
 	public static void main (String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation");
-	    //Variables
-	    int empCheck=(int)(Math .random()*10%3);
-	    int empHrs;
-	    int salary=0;
-	    switch(empCheck)
-	    {
-		   case IS_FULL_TIME:
-		   empHrs=8;
-		   break;
+	   //Variables
+	   int empHrs=0;
+		int totalEmpHrs=0;
+	   int salary=0;
+		int day=0;
+		for(day=0;day<NUM_OF_WORKING_DAYS;day++)
+		{
+			int empCheck=(int)(Math .random()*10%3);
 
-		   case IS_PART_TIME:
-		   empHrs=4;
-		   break;
+			    switch(empCheck)
+			    {
+				   case IS_FULL_TIME:
+				   empHrs=8;
+				   break;
 
-		   default:
-		   empHrs=0;
-	    }
-		salary=empHrs*EMP_RATE_PER_HR;
-		System.out.println("Employee wage:"+salary);	
+				   case IS_PART_TIME:
+				   empHrs=4;
+				   break;
+
+				   default:
+				   empHrs=0;
+			    }
+			totalEmpHrs+=empHrs;
+		}
+		salary=totalEmpHrs*EMP_RATE_PER_HR;
+		System.out.println("Employee wage :"+salary);	
 	}
 }
 
