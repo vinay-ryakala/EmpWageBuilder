6d5ea0ddec08368df56f50c176982f94feeb19f7
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-29 13:35:15 +0530

    ADDING: part time to code and calculating emp wage EmpWagePartTimeUC3

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,17 +4,27 @@ public class EmpWageBuilder {
 
 		System.out.println("Welcome to Employee Wage Computation");
 		int EMP_RATE_PER_HR=20;
-		int PRESENT=1;
+		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
+
 		int empHrs=0;
 		int empWage=0;
-		int attendanceCheck=(int)(Math .random()*10%2);
+		int empCheck=(int)(Math .random()*10%3);
+      if(empCheck==IS_FULL_TIME)
+               {
+                       empHrs=8;
+               }
+      else if(empCheck==IS_PART_TIME)
+               {
+                       empHrs=4;
+               }
+      else
+               {
+                       empHrs=0;
+               }
+      empWage=empHrs*EMP_RATE_PER_HR;
+      System.out.println("Employee wage:" +empWage);
 
-		if (attendanceCheck==PRESENT)
-			empHrs=8;
-		else
-			empHrs=0;
-		empWage=empHrs*EMP_RATE_PER_HR;
-		System.out.println("Employee Wage: "+empWage);
 	}
 }
 
