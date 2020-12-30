cc657194e22877dc1825cd14bb63ea4a9979bdcc
vinay-ryakala <vinayrakyala@gmail.com>
2020-12-30 14:47:56 +0530

    REFACTOR: To use instance variable insted of function parameters UC9

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,12 +5,24 @@ public class EmpWageBuilder
 	public static final int IS_FULL_TIME=1;
 	public static final int IS_PART_TIME=2;
 
-	public static void computeEmpWage(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth)
+	private final String company;
+	private final int empRatePerHr;
+	private final int numberOfWorkingDays;	
+	private final int maxHrPerMonth;
+	private int salary;
+	public EmpWageBuilder(String company, int empRatePerHr, int numberOfWorkingDays, int maxHrPerMonth)
+	{
+		this.company = company;
+		this.empRatePerHr = empRatePerHr;
+   	this.numberOfWorkingDays = numberOfWorkingDays;
+		this.maxHrPerMonth = maxHrPerMonth;
+	}
+
+	public  void computeEmpWage()
 	{
 		//Variables
       int empHrs=0;
       int totalEmpHrs=0;
-      int salary=0;
       int totalWorkingDays=0;
 
       while (totalEmpHrs<maxHrPerMonth && totalWorkingDays<numberOfWorkingDays)
@@ -34,14 +46,22 @@ public class EmpWageBuilder
       }
       salary=totalEmpHrs*empRatePerHr;
       System.out.println("Number of working days:"+totalWorkingDays+" and Total emp hrs: "+totalEmpHrs);
-      System.out.println("Employee wage for company :"+company+"is :"+salary);
-
+      
 	}
+
+	public String toString()
+	{
+		return "Total Emp Wage for company: "+company+" is:"+salary;
+	} 
+
 	public static void main (String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation");
-	   computeEmpWage("swiggy",30,5,10);
-		computeEmpWage("zomato",40,8,20);
+	   EmpWageBuilder swiggy = new EmpWageBuilder("swiggy",30,5,10);
+		EmpWageBuilder zomato = new EmpWageBuilder("zomato",40,8,20);
+		swiggy.computeEmpWage();
+		System.out.println(swiggy);
+		zomato.computeEmpWage();
+      System.out.println(zomato);
 	}
 }
-
